b78421ea4256bd3b727cff088022fae7a7946c16
VijaiVenkatesan <92427958+VijaiVenkatesan@users.noreply.github.com>
2026-03-23 22:48:39 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+# Create logger
+logger = logging.getLogger('competitor_intelligence')
+logger.setLevel(logging.INFO)
